2575b41d9b1b380f229e15455c9668b599138045
William Sams III <w.sams11@yahoo.com>
2020-01-06 09:39:52 -0700

    heroku fix

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require("path");
 app.use(express.static("build"));
 
 app.get("*", function (req, res) {
-    res.sendFile(path.resolve(__dirname, "build/index.html"));
+    res.sendFile(path.resolve(__dirname + "build/index.html"));
 });
 
 app.listen(PORT, function () {
